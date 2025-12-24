4a0c493283c6287b3b7e52f4b87695472ace52d5
Aman <amandeep3771.beai23@chitkara.edu.in>
2025-12-25 00:07:00 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import {
-  Header,
   Footer,
   HeroSection,
   FeaturesSection,
   HowItWorksSection,
   CTASection,
 } from "@/components/landing"
+import { AppHeader } from "@/components/AppHeader"
 
 export default function LandingPage() {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-background to-background/95">
-      <Header />
+      <AppHeader maxWidth="6xl" />
       <main className="flex-1">
         <HeroSection />
         <FeaturesSection />
@@ -21,3 +21,4 @@ export default function LandingPage() {
     </div>
   )
 }
+
